494ae47263c5980914f19f8f6de96681400cf4c8
Emirhan Gerçeker <emirhan@scihook.org>
2024-10-11 21:06:46 +0300

    build(npm): install cheerio

--- src/functions/get-html.ts
+++ src/functions/get-html.ts
@@ -2,6 +2,6 @@ import {readFileSync} from 'fs';
 
 export async function send_html(name:string) {
     var html_buffer = await readFileSync(`html/${name}`)    
-    var html_string = `${html_buffer}`
-    
+    var html_string = `${html_buffer}`;
+    console.log(html_string);
 }
